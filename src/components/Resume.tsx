97ec553f8390869e6c93edcf053b19a76113225b
Maawiah <maviaqaiser09@gmail.com>
2025-07-17 22:17:32 +0500

    Update Resume.tsx

--- src/components/Resume.tsx
+++ src/components/Resume.tsx
@@ -97,6 +97,13 @@ const Resume = () => {
       url: "https://www.coursera.org/account/accomplishments/verify/PE8I2IB6AS0B",   
       image: "/imgs_crf/Python C-2-1.png", 
       date: "2024" 
+    },
+    {
+      name: "Python Data Structures",
+      provider: "Coursera",
+      url: "https://www.coursera.org/account/accomplishments/verify/PE8I2IB6AS0B",   
+      image: "/imgs_crf/Python C-2-1.png", 
+      date: "2024" 
     }
   ];
 
